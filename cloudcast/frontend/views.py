fa4f73fafaa197872f0819a187fc1929ed29abae
Ninad Daithankar <ninad.nd15@gmail.com>
2021-06-07 13:19:36 +0530

    Completed backend model wiring

--- cloudcast/frontend/views.py
+++ cloudcast/frontend/views.py
@@ -1,7 +1,11 @@
 import os
-
+import numpy as np
+import torch
 from django.shortcuts import render
 from .forms import UploadFileForm
+from .cloudcast_autoencoder import runModel, save_images_cartopy
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # Create your views here.
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -15,13 +19,17 @@ def upload_file(request):
                 os.mkdir(os.path.join(current_dir, 'uploads'))
 
             path = os.path.join(current_dir, 'uploads',
-                                str(request.FILES['file']))
+                                'sample.npy')
 
             with open(path, 'wb+') as destination:
                 for chunk in request.FILES['file'].chunks():
                     destination.write(chunk)
             print('Successfully uploaded')
+            img_X = np.load(os.path.join(current_dir, 'uploads/sample.npy'))
+            save_images_cartopy(prediction_video=torch.tensor(img_X),
+                                lines=False, high_res_map=False, name='output', path='./static/uploaded')
+            runModel(img_X)
 
     else:
         form = UploadFileForm()
-    return render(request, 'frontend/index.html', {'form': form, 'values': [0, 1, 2, 3]})
+    return render(request, 'frontend/index.html', {'form': form})
